3e7cc654eda3cba80097e8e4e54aa8ef1f2f15a0
AnonimousZero22 <64372807+AnonimousZero22@users.noreply.github.com>
2023-10-15 20:30:50 -0300

    commit inicial: preparacao da lógica do jogo

---
+++ settings_map.py
@@ -0,0 +1,14 @@
+level_map = [
+'                                  ',
+'                                  ',
+'                                  ',
+'                                  ',
+'                                  ',
+'                                  ',
+'                          X       ',
+'XXXXXXXX  XXXXXXXXXXXXX  XXXXXXXXX',
+]
+
+tile_size = 64
+screen_width = 1200
+screen_height = tile_size * len(level_map)
