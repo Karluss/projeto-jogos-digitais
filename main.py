f5b0182105bcd74904a6fab9f825b3fe35aefdd9
AnonimousZero22 <64372807+AnonimousZero22@users.noreply.github.com>
2023-11-11 21:42:06 -0300

    adc lógica de animação

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
 level = Level(level_map,screen)
 
-while True:
+while True:     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
--- player.py
+++ player.py
@@ -1,12 +1,13 @@
 import pygame
 from support import import_folder
+from settings_map import screen_width, screen_height
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__()  
         self.import_character_assets()
         self.frame_index = 0
-        self.animation_speed = 0.3
+        self.animation_speed = 0.2
         self.image = self.animations['idle'][self.frame_index]
         self.rect = self.image.get_rect(topleft = pos)
         
@@ -15,6 +16,9 @@ class Player(pygame.sprite.Sprite):
         self.speed = 5
         self.gravity = 0.8
         self.jump_speed = -12
+
+        self.status = 'idle'
+        self.facing_right = True
 
     def import_character_assets(self): 
         character_path = 'assets/graphics/character/'  
@@ -25,21 +29,29 @@ class Player(pygame.sprite.Sprite):
             self.animations[animation] = import_folder(full_path)
     
     def animate(self): 
-        animation = self.animations['run']
+        animation = self.animations[self.status]
 
         self.frame_index += self.animation_speed 
         if self.frame_index >= len(animation):
             self.frame_index = 0
+        
+        image = animation[int(self.frame_index)]
+        if self.facing_right:
+            self.image = image
+        else:
+            flipped_image = pygame.transform.flip(image,True, False)
+            self.image = flipped_image
 
-        self.image = animation[int(self.frame_index)]
 
     def get_input(self):
         keys = pygame.key.get_pressed()  
 
         if keys[pygame.K_RIGHT]: 
             self.direction.x = 1
+            self.facing_right = True
         elif keys[pygame.K_LEFT]:
             self.direction.x = -1
+            self.facing_right = False
         else: 
             self.direction.x = 0
         
@@ -53,9 +65,27 @@ class Player(pygame.sprite.Sprite):
         self.direction.y += self.gravity
         self.rect.y += self.direction.y
     
+    def get_status(self): 
+        if self.direction.y < 0:
+            self.status = 'jump'
+        elif self.direction.y > 1:
+            self.status = 'idle'
+        else:
+            if self.direction.x != 0:
+                self.status = 'run'
+            else:
+                self.status = 'idle'
+
+    def resize_image(self):
+        original_image = self.animations[self.status][int(self.frame_index)]
+        resized_image = pygame.transform.scale(original_image, (screen_width/10, screen_height/10))
+        self.image = resized_image
+
     def jump(self): 
         self.direction.y = self.jump_speed
     
     def update(self):
         self.get_input()
+        self.get_status()
+        self.resize_image()
         self.animate()
